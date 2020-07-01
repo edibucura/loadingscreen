c50ed90168839281693186ba405ddc1e1ab0202f
edibucura <edibucura@gmail.com>
2020-07-01 23:20:05 +0300

    .

---
+++ __resource.lua
@@ -0,0 +1,22 @@
+files {
+    'index.html',
+    'ajax/libs/gsap/latest/tweenmax.min.js',
+    'ajax/libs/jquery/1-11-3/jquery.min.js',
+    'ajax/libs/three-js/r71/three.min.js',
+    'config/default.json',
+    'config/fx.json',
+    'css/style.css',
+    'js/lib.min.js',
+    'js/main.min.js',
+    'js/metro.min.js',
+    'npm/clipboard_2/dist/clipboard.min.js',
+    'res/img/intro/loader.gif',
+    'res/light-leak/0.jpg',
+    'res/mp3/Candy.mp3',
+    'res/tunnel/0.jpg',
+    'res/tunnel/1.jpg',
+    'res/tunnel/2.jpg',
+    'res/particle.png',
+}
+
+loadscreen 'index.html'
